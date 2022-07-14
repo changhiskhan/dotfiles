378bc5a043bbde266c83c31ea63ef9f9d097ad25
Chang She <759245+changhiskhan@users.noreply.github.com>
2022-07-14 01:44:58 -0700

    node and desktop utils

--- bootstrap/nodejs.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.zshrc
-nvm install --lts
--- bootstrap/setup.sh
+++ bootstrap/setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Installing base libs and tools"
-sudo apt update
-sudo apt install -y \
-     build-essential
-
 echo "Install sdkman"
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -13,13 +8,5 @@ sdk version
 echo "Install python poetry"
 curl -sSL https://install.python-poetry.org | python3 -
 
-
-conda install -c conda-forge gh  # github cli
-
 . terraform.sh
 . jvm.sh
-. nodejs.sh
-
-if [ -n "$XDG_CURRENT_DESKTOP" ]; then
-  . desktop.sh
-fi
