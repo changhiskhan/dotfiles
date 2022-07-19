3359f56a6cec41929e91bc6c93aa5c3bfb2746be
Chang She <759245+changhiskhan@users.noreply.github.com>
2022-07-18 18:51:07 -0700

    fix up conda installer

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,10 +14,14 @@ export ANSIBLE_STDOUT_CALLBACK=debug
 
 if sudo -n true 2> /dev/null; then
     # for codespaces
-    ansible-playbook main.yml -vv -i hosts --skip-tags ssh,docker
-    conda config --set changeps1 False
+    ansible-playbook main.yml -vv -i hosts
     mkdir -p ~/.emacs.d/
     ln -s init.el ~/.emacs.d/init.el
+    conda init zsh  # conda path is diff    
+    conda config --set changeps1 False
+    conda install -y -c conda-forge gh    
+    # gh auth git-credential
+    # AWS creds using secrets
 else
     read -s -p "Enter a ssh-key passphrase: " ssh_passphrase
     ansible-playbook main.yml -vv --ask-become-pass -i hosts \
