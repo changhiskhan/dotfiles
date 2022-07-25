aaa070e8dd7d87b46ec3a450c218781d3fce8440
Chang She <759245+changhiskhan@users.noreply.github.com>
2022-07-24 19:09:15 -0700

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,19 @@ pushd ansible
 export PATH=~/.local/bin:$PATH  # for ansible galaxy
 ansible-galaxy install -r requirements.yml
 
+OUTPUT=$(basename "$(git rev-parse --show-toplevel)" || echo "NOT_REPO")
+if [ "$OUTPUT" == "NOT_REPO" ]; then
+  git clone https://github.com/changhiskhan/dotfiles
+  pushd dotfiles
+fi
+
+pushd ansible
 export ANSIBLE_STDOUT_CALLBACK=debug
 if sudo -n true 2> /dev/null; then
     ansible-playbook main.yml -vv
+elif [ -z "$PS1" ]; then
+    echo "Need sudo password but not in interactive prompt"
+    exit 1
 else
-    set -i
     ansible-playbook main.yml -vv --ask-become-pass
 fi
